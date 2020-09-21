a2482b1c0b89c12929ded331031f8134a80e7306
madiwa Simon <mad.simon01@gmail.com>
2020-09-21 18:16:19 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def #parrot (phrase = "Squawk!")
- parrot = phrase
+def parrot (parrot = "Squawk!")
+ 
 puts phrase = parrot
 
 end
